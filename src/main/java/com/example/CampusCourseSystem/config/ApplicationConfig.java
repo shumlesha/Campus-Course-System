6d8de1e24a0f499f874f909903e694f20a1ef569
Alexey Shumkov <shumleshas@gmail.com>
2024-02-25 20:29:27 +0700

    Добавлена swagger-документация

--- src/main/java/com/example/CampusCourseSystem/config/ApplicationConfig.java
+++ src/main/java/com/example/CampusCourseSystem/config/ApplicationConfig.java
@@ -2,6 +2,11 @@ package com.example.CampusCourseSystem.config;
 
 import com.example.CampusCourseSystem.security.JwtTokenFilter;
 import com.example.CampusCourseSystem.security.JwtTokenProvider;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
@@ -36,6 +41,17 @@ public class ApplicationConfig {
         return authenticationConfiguration.getAuthenticationManager();
     }
 
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .components(
+                        new Components()
+                                .addSecuritySchemes("bearerAuth", new SecurityScheme().type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer").bearerFormat("JWT"))
+                )
+                .info(new Info().title("Campus System API").description("Campus System Application").version("1.0"));
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
@@ -58,7 +74,11 @@ public class ApplicationConfig {
                             }))
                         )
                 .authorizeHttpRequests(configurer ->
-                        configurer.requestMatchers("/api/accounts/login", "/api/accounts/registration", "/api/accounts/refresh").permitAll()
+                        configurer.requestMatchers("/api/accounts/login",
+                                        "/api/accounts/registration",
+                                        "/api/accounts/refresh",
+                                        "/swagger-ui/**",
+                                        "/v3/api-docs/**").permitAll()
                                 .anyRequest().authenticated()
                         )
 
--- src/main/java/com/example/CampusCourseSystem/controller/AccountController.java
+++ src/main/java/com/example/CampusCourseSystem/controller/AccountController.java
@@ -7,6 +7,8 @@ import com.example.CampusCourseSystem.models.User;
 import com.example.CampusCourseSystem.security.JwtEntity;
 import com.example.CampusCourseSystem.services.AuthService;
 import com.example.CampusCourseSystem.services.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +16,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "Account")
 @Slf4j
 @RestController
 @RequestMapping("/api/accounts")
@@ -23,31 +26,36 @@ public class AccountController {
     private final UserService userService;
     private final AuthService authService;
     private final UserMapper userMapper;
+
+    @Operation(summary = "Register new user")
     @PostMapping("/registration")
-    public ResponseEntity<?> registerUser(@Validated(OnCreate.class) @RequestBody UserRegisterModel userRegisterModel) {
+    public ResponseEntity<TokenResponse> registerUser(@Validated(OnCreate.class) @RequestBody UserRegisterModel userRegisterModel) {
         User user = userMapper.toEntity(userRegisterModel);
         TokenResponse tokenResponse = authService.register(user);
         log.info("token created");
         return ResponseEntity.ok(tokenResponse);
     }
 
+    @Operation(summary = "Log into the system")
     @PostMapping("/login")
-    public ResponseEntity<?> loginUser(@Validated @RequestBody UserLoginModel userLoginModel) {
+    public ResponseEntity<TokenResponse> loginUser(@Validated @RequestBody UserLoginModel userLoginModel) {
 
         return ResponseEntity.ok(authService.login(userLoginModel));
     }
 
+    @Operation(summary = "Log out system user")
     @PostMapping("/logout")
     public ResponseEntity<?> logoutUser() {
         return ResponseEntity.ok().body(new Response());
     }
 
+    @Operation(summary = "Get current user's profile info")
     @GetMapping("/profile")
-    public ResponseEntity<?> getUserProfile(@AuthenticationPrincipal JwtEntity jwtEntity) {
+    public ResponseEntity<UserRegisterModel> getUserProfile(@AuthenticationPrincipal JwtEntity jwtEntity) {
         return ResponseEntity.ok(userMapper.toDto(userService.getById(jwtEntity.getId())));
     }
 
-
+    @Operation(summary = "Edit current user's profile info")
     @PutMapping("/profile")
     public ResponseEntity<?> editUserProfile (@AuthenticationPrincipal JwtEntity jwtEntity, @RequestBody EditUserProfileModel editUserProfileModel) {
 
